1828620ffdcd241388f8502c8574bc02957e39ac
Adotey Acquaye <adoteya2@vt.edu>
2017-03-24 23:19:43 +0000

    Updated contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,22 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+            
+            if @contact.save
+                flash[:infoggi] = 'Message sent.'
+                redirect_to new_contact_path
+            else
+                flash[:error] = 'Message not sent, error occured.' 
+                redirect_to new_contact_path
+            end
     end
+
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :message)
+        end
 
 end
