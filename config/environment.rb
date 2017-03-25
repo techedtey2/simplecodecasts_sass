5eeb6ffa002b29e1550ad56fcb0bae6eef270486
Adotey Acquaye <adoteya2@vt.edu>
2017-03-25 02:57:00 +0000

    Updated environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
-require_relative 'application'
+require File.expand_path('../application', _FILE_)
 
 # Initialize the Rails application.
 Rails.application.initialize!
