c598505b09dbd220e072abad1832164f71da23c7
Adotey Acquaye <adoteya2@vt.edu>
2017-04-02 15:07:45 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,5 +5,21 @@ class ProfilesController < ApplicationController
         @model = Model.find( params[:model_id] )
         @profile = @model.build_profile
     end
-
+    
+    def create
+       @model = Model.find(params[:model_id])
+    #   bind to user who just filled out form.
+       @profile = @model.build_profile(profile_params)
+        if @profile.save
+           flash[:success] = "Profile updated!"
+           redirect_to model_path(params[:model_id])
+       else
+          render action: :new 
+        end
+    end
+     
+    private
+    def profile_params
+       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description) 
+    end
 end
