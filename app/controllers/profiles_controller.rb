879ac8b15c66d7a425158d0efa3349059f34b7e1
Adotey Acquaye <adoteya2@vt.edu>
2017-04-02 03:45:25 +0000

    Added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
-Class ProfilesController < ApplicationController
-  
+class ProfilesController < ApplicationController
+
+    def new
+        # form where a user can fill out their own profile.
+        @model = Model.find( params[:model_id] )
+        @profile = @model.build_profile
+    end
+
 end
